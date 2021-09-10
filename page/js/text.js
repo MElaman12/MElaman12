10c297cefc4ac54ca456fff92c87428ecfd18c0c
MElaman12 <shahram1223334444@gmail.com>
2021-09-10 21:29:44 +0500

    Online Eda

--- page/js/text.js
+++ page/js/text.js
@@ -5,6 +5,32 @@ let pizzaStats = [
     }
 ];
 
+let saladStats = [
+    {
+        name: 'Салат цезарь',
+        storage: '1л',
+        price: 15000
+    },
+    {
+        name: 'Азия',
+        storage: '0.5',
+        price: 18000
+    },
+    {
+        name: 'Смак',
+        storage: '1л',
+        price: 12000
+    },
+]
+
+let saladTitle = document.querySelector('.salad-menu .salads .first-salad .title');
+
+
+for(let i = 0; saladStats.length > 0; i++){
+    saladTitle.textContent = saladStats[0].name;
+};
+
+// ------
 let ingridients = document.querySelectorAll('.ingridients');
 let price = document.querySelectorAll('.price');
 
