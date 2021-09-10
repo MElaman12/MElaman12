e0478d75c3606c8bf1547dc5937985b37b42f564
MElaman12 <shahram1223334444@gmail.com>
2021-09-10 21:56:58 +0500

    Online Eda

--- page/js/text.js
+++ page/js/text.js
@@ -5,37 +5,38 @@ let pizzaStats = [
     }
 ];
 
-let saladStats = [
-    {
-        name: 'Салат цезарь',
-        storage: '1л',
-        price: 15000
-    },
-    {
-        name: 'Азия',
-        storage: '0.5',
-        price: 18000
-    },
-    {
-        name: 'Смак',
-        storage: '1л',
-        price: 12000
-    },
-]
+// let saladStats = [
+//     {
+//         name: 'Салат цезарь',
+//         storage: '1л',
+//         price: 15000
+//     },
+//     {
+//         name: 'Азия',
+//         storage: '0.5',
+//         price: 18000
+//     },
+//     {
+//         name: 'Смак',
+//         storage: '1л',
+//         price: 12000
+//     },
+// ];
 
-let saladTitle = document.querySelector('.salad-menu .salads .first-salad .title');
+// let saladTitle = document.querySelector('.salad-menu .salads .first-salad .title');
 
 
-for(let i = 0; saladStats.length > 0; i++){
-    saladTitle.textContent = saladStats[0].name;
-};
+// for(let i = 0; saladStats.length > 0; i++){
+//     saladTitle.textContent = saladStats[0].name;
+// };
 
 // ------
 let ingridients = document.querySelectorAll('.ingridients');
-let price = document.querySelectorAll('.price');
+
+let price = document.querySelectorAll('.first-pizza .price');
 
 for(let i = 0; ingridients.length > 0; i++){
-    price[i].textContent = pizzaStats[0].price;
+    price[i].textContent = pizzaStats[0].price
     ingridients[i].textContent = pizzaStats[0].ingridients;
 }
 
