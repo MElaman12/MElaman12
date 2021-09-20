16a68fac71d12d199e63eb5fe462a015a16a6976
MElaman12 <shahram1223334444@gmail.com>
2021-09-20 21:22:39 +0500

    Enline Eda

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let price = document.querySelectorAll('.first-pizza .price');
 let signBtn = document.querySelector('.sign');
 let blurBlock  = document.querySelector('.blur-block')
 
+
 signBtn.onclick = () => {
     event.preventDefault();
     blurBlock.style.top = '0%'
@@ -19,6 +20,9 @@ let bigPizza = document.querySelectorAll('.big')
 let pizzaImg = document.querySelectorAll('.first-pizza img')
 let pore = document.querySelectorAll('.pore')
 
+let a = '80px'
+let b = '40px'
+let c = '0px'
 
 smallPizza[0].onclick = () => {
    PizzaPrice[0].textContent = '35000 сум'
@@ -27,24 +31,30 @@ smallPizza[0].onclick = () => {
 
     let  Interval =  setInterval( () => {
          
-      if(pore[0].style.marginBottom == '40px'){
+      if(price[0].textContent == '35000 сум' && c == pore[0].style.marginBottom || pore[0].style.marginBottom == '' ){
          width--
          height--
          pizzaImg[0].style.width = `${width}px`
          pizzaImg[0].style.height = `${height}px`
          if(width == 140){
-            pore[0].style.marginBottom = '80px'
+            smallPizza[0].style.pointerEvents = 'none';
+            averagePizza[0].style.pointerEvents = 'auto';
+            bigPizza[0].style.pointerEvents = 'auto';
+            pore[0].style.marginBottom = '80px';
             clearInterval(Interval);
             };
       }
       
-      if(pore[0].style.marginBottom == 0){
+      if(price[0].textContent == '35000 сум' && b == pore[0].style.marginBottom){
           width--
           height--
           pizzaImg[0].style.width = `${width}px`
           pizzaImg[0].style.height = `${height}px`
 
           if(width == 140){
+            smallPizza[0].style.pointerEvents = 'none';
+            averagePizza[0].style.pointerEvents = 'auto';
+            bigPizza[0].style.pointerEvents = 'auto';
              pore[0].style.marginBottom = '80px'
              clearInterval(Interval);
              };
@@ -58,14 +68,14 @@ averagePizza[0].onclick = () => {
       let height = pizzaImg[0].clientHeight;
 
       let  Interval =  setInterval( () => {
-         if( pore[0].style.marginBottom == ''){
+         if(pore[0].style.marginBottom ==  '' || c == pore[0].style.marginBottom){
             width--
             height--
             pizzaImg[0].style.width = `${width}px`
             pizzaImg[0].style.height = `${height}px`
          }
 
-         if(pore[0].style.marginBottom == '80px'){
+         if(pore[0].style.marginBottom == a){
             width++
             height++
             pizzaImg[0].style.width = `${width}px`
@@ -73,235 +83,36 @@ averagePizza[0].onclick = () => {
            } 
            
             if(width == 180){
+               averagePizza[0].style.pointerEvents = 'none';
+               smallPizza[0].style.pointerEvents = 'auto';
+               bigPizza[0].style.pointerEvents = 'auto';
                pore[0].style.marginBottom = '40px'
                clearInterval(Interval)
             }
-         }, 10)
+         }, 5)
 }
 
 bigPizza[0].onclick = () => {
+
    PizzaPrice[0].textContent = '80000 сум'
    let width = pizzaImg[0].clientWidth;
    let height = pizzaImg[0].clientHeight;
 
    let Interval = setInterval( () => {
-   if(pore[0].style.marginBottom == '80px'){
-      width++
-      height++
-      pizzaImg[0].style.width = `${width}px`
-      pizzaImg[0].style.height = `${height}px`
 
+      if(price[0].textContent == '80000 сум' && b == pore[0].style.marginBottom ||  a == pore[0].style.marginBottom){
+         width++
+         height++
+         pizzaImg[0].style.width = `${width}px`
+         pizzaImg[0].style.height = `${height}px`
+      }
+         
       if(width == 220){
-         pore[0].style.marginBottom == '0px';
+         smallPizza[0].style.pointerEvents = 'auto';
+         averagePizza[0].style.pointerEvents = 'auto';
+         bigPizza[0].style.pointerEvents = 'none';
+         pore[0].style.marginBottom = c;
          clearInterval(Interval);
       }
-   }
-
-   if(pore[0].style.marginBottom == '40px'){
-      width++
-      height++
-      pizzaImg[0].style.width = `${width}px`
-      pizzaImg[0].style.height = `${height}px`
-
-      if(width == 220){
-         pore[0].style.marginBottom == '0px';
-         clearInterval(Interval);
-      }
-   }
 }, 5)
-
-}   
-//    smallPizza[1].onclick = () => {
-//       PizzaPrice[1].textContent = '35000 сум'
-//     let width = pizzaImg[1].clientWidth;
-//     let height = pizzaImg[1].clientHeight;
-
-//     let  Interval =  setInterval( () => {
-        
-//       if(pizzaImg[1].classList.value == 'pizza-img' || pizzaImg[1].classList.value == 'pizza-img1' || pizzaImg[1].classList.value == 'big1' || pizzaImg[1].classList.value == 'pizza-img big1' || pizzaImg[1].classList.value == 'pizza-img pizza-img1' ){
-//           width--
-//           height--
-//           pizzaImg[1].style.width = `${width}px`
-//           pizzaImg[1].style.height = `${height}px`
-//       } 
-//    }, 10)
-      
-//    let Interval2 = setInterval( () => {
-//       if(width == 140){
-//          pizzaImg[1].classList.add('averageScale');
-//          pizzaImg[1].classList.remove('pizza-img');
-//          pizzaImg[1].classList.remove('big1');
-//          pore[1].style.marginBottom = '80px'
-//          clearInterval(Interval);
-//          clearInterval(Interval2);
-//          };
-//       }, 10)
-//    }
-
-//    smallPizza[2].onclick = () => {
-//       PizzaPrice[2].textContent = '35000 сум'
-//        let width = pizzaImg[2].clientWidth;
-//        let height = pizzaImg[2].clientHeight;
-   
-//        let  Interval =  setInterval( () => {
-          
-//          if(pizzaImg[2].classList.value == 'pizza-img' || pizzaImg[2].classList.value == 'pizza-img1' || pizzaImg[2].classList.value == 'big1' || pizzaImg[2].classList.value == 'pizza-img big1' || pizzaImg[2].classList.value == 'pizza-img pizza-img1' ){
-//              width--
-//              height--
-//              pizzaImg[2].style.width = `${width}px`
-//              pizzaImg[2].style.height = `${height}px`
-//          } 
-//       }, 10)
-         
-//       let Interval2 = setInterval( () => {
-//          if(width == 140){
-//             pizzaImg[2].classList.add('averageScale');
-//             pizzaImg[2].classList.remove('pizza-img');
-//             pizzaImg[2].classList.remove('big1');
-//             pore[2].style.marginBottom = '80px'
-//             clearInterval(Interval);
-//             clearInterval(Interval2);
-//             };
-//          }, 10)
-//    }
-
-// averagePizza[1].onclick = () => {
-//    PizzaPrice[1].textContent = '57000 сум'
-//    let width = pizzaImg[1].clientWidth;
-//    let height = pizzaImg[1].clientHeight;
-
-//    let  Interval =  setInterval( () => {
-
-//       if( pizzaImg[1].classList.value == 'pizza-img' && pore[1].style.marginBottom == 0){
-//          width--
-//          height--
-//          pizzaImg[1].style.width = `${width}px`
-//          pizzaImg[1].style.height = `${height}px`
-//       }
-
-//       if(pizzaImg[1].classList.value == 'averageScale' || pizzaImg[1].style.width == '140px'){
-//          width++
-//          height++
-//          pizzaImg[1].style.width = `${width}px`
-//          pizzaImg[1].style.height = `${height}px`
-//         } 
-//       }, 10)
-     
-//       let Interval2 = setInterval( () => {
-//          if(pizzaImg[1].style.width == '180px'){
-//             pore[1].style.marginBottom = '40px'
-//             pizzaImg[1].classList.add('pizza-img1')
-//             pizzaImg[1].classList.remove('averageScale')
-//             pizzaImg[1].classList.remove('big1')
-//             clearInterval(Interval)
-//             clearInterval(Interval2)
-            
-//             if(pizzaImg[1].style.width == '180px' && pizzaImg[1].style.height == '220px' || pizzaImg[1].style.height == '141px'){
-//                pizzaImg[1].style.height = '180px'
-//             }
-//          }
-// },10)
-// }
-
-
-// averagePizza[2].onclick = () => {
-//    PizzaPrice[2].textContent = '57000 сум'
-//    let width = pizzaImg[2].clientWidth;
-//    let height = pizzaImg[2].clientHeight;
-
-//    let  Interval =  setInterval( () => {
-//        if( pizzaImg[2].classList.value == 'pizza-img' && pore[2].style.marginBottom >= 0){
-//          width--
-//          height--
-//          pizzaImg[2].style.width = `${width}px`
-//          pizzaImg[2].style.height = `${height}px`
-//       }
-
-//       if(pizzaImg[2].classList.value == 'averageScale' || pizzaImg[2].style.width == '140px'){
-//          width++
-//          height++
-//          pizzaImg[2].style.width = `${width}px`
-//          pizzaImg[2].style.height = `${height}px`
-//         } 
-//       }, 10)
-     
-//       let Interval2 = setInterval( () => {
-//          if(pizzaImg[2].style.width == '180px'){
-//          pore[2].style.marginBottom = '40px'
-//             pizzaImg[2].classList.add('pizza-img1')
-//             pizzaImg[2].classList.remove('averageScale')
-//             pizzaImg[2].classList.remove('big1')
-//             clearInterval(Interval)
-//             clearInterval(Interval2)
-            
-//             if(pizzaImg[2].style.width == '180px' && pizzaImg[2].style.height == '220px' || pizzaImg[2].style.height == '141px'){
-//                pizzaImg[2].style.height = '180px'
-//             }
-//          }
-// },10)
-// }
-
-
-
-// bigPizza[1].onclick = () => {
-//    PizzaPrice[1].textContent = '80000 сум'
-//    let width = pizzaImg[1].clientWidth;
-//    let height = pizzaImg[1].clientHeight;
-
-//    let  Interval =  setInterval( () => {
-      
-//       if(pizzaImg[1].classList.value == 'pizza-img1' && pizzaImg[1].style.width == '180px' || pizzaImg[1].classList.value == 'averageScale' || pizzaImg[1].classList.value == 'pizza-img big1' || pizzaImg[1].classList.value == 'pizza-img pizza-img1'){
-//          width++
-//          height++
-//          pizzaImg[1].style.width = `${width}px`
-//          pizzaImg[1].style.height = `${height}px`
-//          pore[1].style.marginBottom = 0
-         
-//       } 
-//    }, 10)
-   
-//    if(pore[1].style.marginBottom >= '40px'){
-//       pore[1].style.marginBottom = 0
-//    }
-
-//    let Interval2 = setInterval( () => {
-//       if(pizzaImg[1].style.width == '220px'){
-//          pizzaImg[1].classList.remove('pizza-img1')
-//          pizzaImg[1].classList.remove('averageScale')
-//          pizzaImg[1].classList.add('big1')
-//          clearInterval(Interval)
-//          clearInterval(Interval2)
-//       }
-//    },10)
-// }
-
-// bigPizza[2].onclick = () => {
-//    PizzaPrice[2].textContent = '80000 сум'
-//    let width = pizzaImg[2].clientWidth;
-//    let height = pizzaImg[2].clientHeight;
-
-//    let  Interval =  setInterval( () => {
-//       if(pizzaImg[2].classList.value == 'pizza-img1' && pizzaImg[2].style.width == '180px' || pizzaImg[2].classList.value == 'averageScale' || pizzaImg[2].classList.value == 'pizza-img big1' || pizzaImg[2].classList.value == 'pizza-img pizza-img1'){
-//          width++
-//          height++
-//          pizzaImg[2].style.width = `${width}px`
-//          pizzaImg[2].style.height = `${height}px`
-//          pore[2].style.marginBottom = 0
-         
-//       } 
-//    }, 10)
-   
-//    if(pore[2].style.marginBottom >= '40px'){
-//       pore[2].style.marginBottom = 0
-//    }
-
-//    let Interval2 = setInterval( () => {
-//       if(pizzaImg[2].style.width == '220px' || pizzaImg[2].classList.value == 'big1'){
-//          pizzaImg[2].classList.remove('pizza-img1')
-//          pizzaImg[2].classList.remove('averageScale')
-//          pizzaImg[2].classList.add('big1')
-//          clearInterval(Interval)
-//          clearInterval(Interval2)
-//       }
-//    },10)
-// }
+}
